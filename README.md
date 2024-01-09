1e5e387a8ca9698eec3977d3b66df2b20409773d
root@logs-input <root@logs-input>
2024-01-09 13:54:14 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# dam
-diy alerts manager
+# diy alerts monitor
+
+## requirements
+
+- an elastic/opensearch cluster
+- some logs to be pushed over there (suricata eve with `flow` enabled as an alert sample)
+- specific fields to be available for alert verbosity (`sensor src.name dest.name` in our example)
+
+## install
+
+	mkdir -p /data/
+	cd /data/
+	git clone ...
+	cd dam/
+	cp -pi dam.conf.sample dam.conf
+	vi dam.conf
+
+	...
+
+setup an alert e.g.
+
+	cp -pi suricata-src1024.conf.sample suricata-src1024.conf
+	vi suricata-src1024.conf
+
+	...
+
+## test
+
+	cd /data/dam/
+	ls -lF *.lock
+	./alert-hit.bash suricata-src1024.conf
+
+## enable
+
+```
+*/15 * * * * /data/dam/alert-hit.bash suricata-src1024.conf >> /var/log/dam.log 2>&1
+```
+
+## resources
+
+https://elastic.co/guide/en/elasticsearch/reference/7.17/query-dsl-range-query.html
+
---
+++ alert-hit.bash
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+function prep_alert {
+        # beware of escapes for "`"
+        cat <<EOF
+$title
+
+\`\`\`
+$index
+Lucene> $query
+\`\`\`
+
+found $hits hits within last $delay_minutes minutes
+$saved_search_url
+
+EOF
+
+	# that is in regards to the first 100 hits
+	# output as one-liners
+	echo sensors: $sensors
+	echo source names: $src_names
+	echo destination names: $dest_names
+
+	cat <<EOF
+
+(throttle for today $day)
+EOF
+}
+
+[[ ! -x `which jq` ]] && echo install jq first && exit 1
+
+[[ -z $1 ]] && echo what alert.conf? && exit 1
+alert_conf=$1
+alert=${alert_conf%\.conf}
+
+[[ ! -r /data/dam/dam.conf ]] && echo cannot read /data/dam/dam.conf && exit 1
+source /data/dam/dam.conf
+
+[[ ! -r /data/dam/$alert_conf ]] && echo cannot read /data/dam/$alert_conf && exit 1
+source /data/dam/$alert_conf
+
+day=`date +%Y-%m-%d`
+lock=/data/dam/$alert.$day.lock
+
+# start logging
+echo `date` - $0 $alert_conf
+
+[[ -f $lock ]] && echo there is a lock already for today \($day\) && exit 0
+
+result=`cat <<EOF | curl -sk -X POST -H "Content-Type: application/json" \
+        "$endpoint/$index/_search?pretty" -u $user:$passwd -d @-
+{
+    "size": 100,
+    "query": {
+        "bool": {
+            "filter": [
+                {
+                    "query_string": {
+                        "query": "$query"
+                    }
+                },
+                {
+                    "range": {
+                        "@timestamp": {
+                            "from": "now-${delay_minutes}m/m",
+                            "to": "now/m"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
+EOF`
+
+hits=`echo "$result" | jq -r ".hits.total.value"`
+
+(( hits < 1 )) && echo no hits \($hits\) we are all good && exit 0
+
+sensors=`echo "$result" | jq -r ".hits.hits[]._source.sensor" | sort -uV`
+
+# no idea yet why this returns null
+#src_names=`echo "$result" | jq -r ".hits.hits[]._source.src.name" | sort -uV`
+src_names=`echo "$result" | grep src.name | sort -uV | cut -f4 -d'"'`
+
+# no idea yet why this returns null
+#dest_names=`echo "$result" | jq -r ".hits.hits[]._source.dest.name" | sort -uV`
+dest_names=`echo "$result" | grep dest.name | sort -uV | cut -f4 -d'"'`
+
+# TODO show IPs when there is no src.name nor dest.name
+# (this would require to fix the null answer from jq)
+
+text=`prep_alert`
+
+touch $lock
+if (( dummy == 1 )); then
+	echo the following would be sent to $webhook
+	echo "$text"
+else
+	echo -n sending webhook to slack ...
+	curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$text\"}" $webhook
+	echo
+fi
+
